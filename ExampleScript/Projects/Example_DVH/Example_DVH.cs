1fa4ee36a9a6c3e00618b4dc880b534c044d2521
pyfdrw <tobepyf@163.com>
2019-08-05 02:28:46 -0700

    print dvh functions were finished

--- ExampleScript/Projects/Example_DVH/Example_DVH.cs
+++ ExampleScript/Projects/Example_DVH/Example_DVH.cs
@@ -6,64 +6,82 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using VMS.TPS.Common.Model.API;
 using VMS.TPS.Common.Model.Types;
+using System.Collections.ObjectModel;
+using Example_DVH;
 
 
 // Do not change namespace and class name
 // otherwise Eclipse will not be able to run the script.
 namespace VMS.TPS
 {
-  class Script
-  {
-    public Script()
+    class Script
     {
-    }
-
-    [MethodImpl(MethodImplOptions.NoInlining)]
-    public void Execute(ScriptContext context, Window window)
-    {
-      PlanSetup planSetup = context.PlanSetup;
-
-      // If there's no selected plan with calculated dose throw an exception
-      if (planSetup == null || planSetup.Dose == null)
-        throw new ApplicationException("Please open a calculated plan before using this script.");
-
-      // Retrieve StructureSet
-      StructureSet structureSet = planSetup.StructureSet;
-      if (structureSet == null)
-        throw new ApplicationException("The selected plan does not reference a StructureSet.");
-
-      // For this example we will retrieve first available structure of PTV type
-      Structure target = null;
-      foreach (var structure in structureSet.Structures)
-      {
-        if (structure.DicomType == "PTV")
+        public Script()
         {
-          target = structure;
-          break;
         }
 
-      }
-      if (target == null)
-        throw new ApplicationException("The selected plan does not have a PTV.");
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public void Execute(ScriptContext context, Window window)
+        {
+            PlanSetup planSetup = context.PlanSetup;
 
-      // Retrieve DVH data
-      DVHData dvhData = planSetup.GetDVHCumulativeData(target,
-                                    DoseValuePresentation.Relative,
-                                    VolumePresentation.Relative, 0.1);
+            // If there's no selected plan with calculated dose throw an exception
+            if (planSetup == null || planSetup.Dose == null)
+                throw new ApplicationException("Please open a calculated plan before using this script.");
 
-      if (dvhData == null)
-        throw new ApplicationException("DVH data does not exist. Script execution cancelled.");
+            // Retrieve StructureSet
+            StructureSet structureSet = planSetup.StructureSet;
+            if (structureSet == null)
+                throw new ApplicationException("The selected plan does not reference a StructureSet.");
 
-      // Add existing WPF control to the script window.
-      var mainControl = new Example_DVH.MainControl();
-      window.Content = mainControl;
-      window.Width = 610;
-      window.Height = 460;
+            // For this example we will retrieve first available structure of PTV type
+            //Structure target = null;
+            //foreach (var structure in structureSet.Structures)
+            //{
+            //    if (structure.DicomType == "PTV")
+            //    {
+            //        target = structure;
+            //        break;
+            //    }
 
-      window.Title = "Plan : " + planSetup.Id + ", Structure : " + target.Id;
+            //}
+            //if (target == null)
+            //    throw new ApplicationException("The selected plan does not have a PTV.");
 
-      // Draw DVH
-      mainControl.DrawDVH(dvhData);
+            // Retrieve DVH data
+            //DVHData dvhData = planSetup.GetDVHCumulativeData(target,
+            //                              DoseValuePresentation.Relative,
+            //                              VolumePresentation.Relative, 0.1);
+
+            //if (dvhData == null)
+            //    throw new ApplicationException("DVH data does not exist. Script execution cancelled.");
+
+            ObservableCollection<string> organList = new ObservableCollection<string>();
+            foreach (Structure stru in planSetup.StructureSet.Structures)
+            {
+                organList.Add(stru.Id);
+            }
+
+            MessageBox.Show(planSetup.ToString());
+
+            // Add existing WPF control to the script window.
+            MainControl mainControl = new Example_DVH.MainControl(organList, planSetup);
+
+            // MessageBox.Show(mainControl.ToString());
+
+            if (!mainControl.IsInitializationFinished)
+                throw new Exception("Unable to init mainControl");
+
+            mainControl.organListComboBox.ItemsSource = organList;
+
+            window.Content = mainControl;
+            window.Width = 800;
+            window.Height = 600;
+
+            // window.Title = "Plan : " + planSetup.Id + ", Structure : " + target.Id;
+
+            // Draw DVH
+            // mainControl.DrawDVH(dvhData);
+        }
     }
-  }
 }
--- ExampleScript/Projects/Example_DVH/MainControl.xaml.cs
+++ ExampleScript/Projects/Example_DVH/MainControl.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -12,44 +13,110 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using VMS.TPS.Common.Model.API;
+using VMS.TPS.Common.Model.Types;
 
 namespace Example_DVH
 {
-  /// <summary>
-  /// Interaction logic for MainControl.xaml
-  /// </summary>
-  public partial class MainControl : UserControl
-  {
-    public MainControl()
+    /// <summary>
+    /// Interaction logic for MainControl.xaml
+    /// </summary>
+    public partial class MainControl : UserControl
     {
-      InitializeComponent();
+
+        ObservableCollection<string> organNames = null;
+        PlanSetup planSetupInUse = null;
+
+        Structure stuSelected = null;
+
+        public bool IsInitializationFinished = false;
+
+        public MainControl(ObservableCollection<string> organList, PlanSetup planIn)
+        {
+            if(null == organList || organList.Count == 0 || planIn == null)
+            {
+                IsInitializationFinished = false;
+                // MessageBox.Show("Unable to init");
+            }
+            else
+            {
+                organNames = organList;
+                planSetupInUse = planIn;
+
+                IsInitializationFinished = true;
+            }
+
+            // MessageBox.Show("before to init");
+            InitializeComponent();
+            // organListComboBox.ItemsSource = organNames;
+        }
+
+        public void DrawDVH()
+        {
+            if (stuSelected == null)
+                return;
+
+            DVHData dvhData = planSetupInUse.GetDVHCumulativeData(stuSelected,
+                                          DoseValuePresentation.Absolute,
+                                          VolumePresentation.Relative, 1);
+
+            // clear 
+            MainCanvas.Children.Clear();
+
+            // Calculate multipliers for scaling DVH to canvas.
+            double xCoeff = MainCanvas.Width / dvhData.MaxDose.Dose;
+            double yCoeff = MainCanvas.Height / 100;
+
+            // Set Y axis label
+            DoseMaxLabel.Content = string.Format("{0:F0} {1}", dvhData.MaxDose.Dose, dvhData.MaxDose.UnitAsString);
+
+            // Draw histogram 
+            for (int i = 0; i < dvhData.CurveData.Length - 1; i++)
+            {
+                // Set drawing line parameters
+                var line = new Line() { Stroke = new SolidColorBrush(stuSelected.Color), StrokeThickness = 2.0 };
+
+                // Set line coordinates
+                line.X1 = dvhData.CurveData[i].DoseValue.Dose * xCoeff;
+                line.X2 = dvhData.CurveData[i + 1].DoseValue.Dose * xCoeff;
+                // Y axis start point is top-left corner of window, convert it to bottom-left.
+                line.Y1 = MainCanvas.Height - dvhData.CurveData[i].Volume * yCoeff;
+                line.Y2 = MainCanvas.Height - dvhData.CurveData[i + 1].Volume * yCoeff;
+                // Add line to the existing canvas
+                MainCanvas.Children.Add(line);
+            }
+
+            // show info 
+            infoText.Text += string.Format(stuSelected.Id + "  Max dose = {0:F2} {1}, mean dose = {2:F2} {3}, min dose = {4:F2} {5}" + Environment.NewLine,
+                dvhData.MaxDose.Dose, dvhData.MaxDose.UnitAsString, 
+                dvhData.MeanDose.Dose, dvhData.MeanDose.UnitAsString,
+                dvhData.MinDose.Dose, dvhData.MinDose.UnitAsString);
+        }
+
+        // calculate dvh from organ Id
+        private int calculateDvhFromOrganId()
+        {
+            if(organListComboBox.SelectedIndex != -1)
+            {
+                var stru =  planSetupInUse.StructureSet.Structures.Where(x => x.Id.Equals(organListComboBox.SelectedItem as string)).First();
+                if (stru == null)
+                    throw new Exception("Could not determin the organ by delected id");
+
+                stuSelected = stru;
+
+                return 0;
+            }
+            else
+            {
+                MessageBox.Show("Please selected one structure");
+                return 1;
+            }
+        }
+
+        // DVH calculation
+        private void calDvhClick(object sender, RoutedEventArgs e)
+        {
+            calculateDvhFromOrganId();
+            DrawDVH();
+        }
     }
-
-    public void DrawDVH(DVHData dvhData)
-    {
-      // Calculate multipliers for scaling DVH to canvas.
-      double xCoeff = MainCanvas.Width / dvhData.MaxDose.Dose;
-      double yCoeff = MainCanvas.Height / 100;
-
-      // Set Y axis label
-      DoseMaxLabel.Content = string.Format("{0:F0}%", dvhData.MaxDose.Dose);
-
-      // Draw histogram 
-      for (int i = 0; i < dvhData.CurveData.Length - 1; i++)
-      {
-        // Set drawing line parameters
-        var line = new Line() { Stroke = Brushes.Blue, StrokeThickness = 4.0 };
-
-        // Set line coordinates
-        line.X1 = dvhData.CurveData[i].DoseValue.Dose * xCoeff;
-        line.X2 = dvhData.CurveData[i + 1].DoseValue.Dose * xCoeff;
-        // Y axis start point is top-left corner of window, convert it to bottom-left.
-        line.Y1 = MainCanvas.Height - dvhData.CurveData[i].Volume * yCoeff;
-        line.Y2 = MainCanvas.Height - dvhData.CurveData[i + 1].Volume * yCoeff;
-
-        // Add line to the existing canvas
-        MainCanvas.Children.Add(line);
-      }
-    }
-  }
 }
