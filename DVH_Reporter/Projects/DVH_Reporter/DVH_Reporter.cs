c907906c0d97daa01ca9f1a229a2eddfd2f84909
pyfdrw <tobepyf@163.com>
2019-08-05 20:01:21 -0700

    ass dvhviewmodel codes

--- DVH_Reporter/Projects/DVH_Reporter/DVH_Reporter.cs
+++ DVH_Reporter/Projects/DVH_Reporter/DVH_Reporter.cs
@@ -18,16 +18,16 @@ using VMS.TPS.Common.Model.Types;
 
 namespace VMS.TPS
 {
-  public class Script
-  {
-    public Script()
+    public class Script
     {
-    }
+        public Script()
+        {
+        }
 
-    [MethodImpl(MethodImplOptions.NoInlining)]
-    public void Execute(ScriptContext context /*, System.Windows.Window window, ScriptEnvironment environment*/)
-    {
-      // TODO : Add here the code that is called when the script is launched from Eclipse.
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public void Execute(ScriptContext context /*, System.Windows.Window window, ScriptEnvironment environment*/)
+        {
+            // TODO : Add here the code that is called when the script is launched from Eclipse.
+        }
     }
-  }
 }
