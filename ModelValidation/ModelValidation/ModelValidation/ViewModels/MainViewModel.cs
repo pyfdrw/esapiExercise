aa760c541d8bec7a2e24beb3d1c0c1323e55a6b0
pyfdrw <tobepyf@163.com>
2019-08-08 00:16:06 -0700

    add plot and export function

--- ModelValidation/ModelValidation/ModelValidation/ViewModels/MainViewModel.cs
+++ ModelValidation/ModelValidation/ModelValidation/ViewModels/MainViewModel.cs
@@ -1,10 +1,12 @@
 ﻿using ModelValidation.Models;
 using OxyPlot;
+using OxyPlot.Series;
 using Prism.Commands;
 using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -45,6 +47,7 @@ namespace ModelValidation.ViewModels
         private string selectedPlan;
 
         public ObservableCollection<ScanData> ScanDataCollection { get; private set; }
+        public DelegateCommand ExportDataCommand { get; private set; }
 
         // ?
         private ICommand closedWindowCommand;
@@ -55,11 +58,59 @@ namespace ModelValidation.ViewModels
             set { SetProperty(ref closedWindowCommand, value); }
         }
 
+        // ViewModel and visibility
+        private Visibility profileVisibility = Visibility.Collapsed;
+        public Visibility ProfileVisibility
+        {
+            get { return profileVisibility; }
+            set
+            {
+                SetProperty(ref profileVisibility, value);
+            }
+        }
+
+        private Visibility pddVisibility = Visibility.Collapsed;
+        public Visibility PDDVisibility
+        {
+            get { return pddVisibility; }
+            set
+            {
+                SetProperty(ref pddVisibility, value);
+            }
+        }
+
+        private PlotModel profilePlotModel = new PlotModel()
+        {
+            Title = "Profile (x)"
+        };
+        public PlotModel ProfilePlotModel
+        {
+            get { return profilePlotModel; }
+            set
+            {
+                SetProperty(ref profilePlotModel, value);
+            }
+        }
+
+        private PlotModel pddPlotModel = new PlotModel()
+        {
+            Title = "PDD"
+        };
+        public PlotModel PDDPlotModel
+        {
+            get { return pddPlotModel; }
+            set
+            {
+                SetProperty(ref pddPlotModel, value);
+            }
+        }
 
         private VMS.TPS.Common.Model.API.Application app;
         private Patient _patient;
         private Course _course;
         private PlanSetup _planSetup;
+
+        public DelegateCommand<object> SelectedItemChangedCommand { get; set; }
 
         public MainViewModel(VMS.TPS.Common.Model.API.Application app)
         {
@@ -72,7 +123,105 @@ namespace ModelValidation.ViewModels
             OpenPatientCommand = new DelegateCommand(OnOpenPatient);
 
             ClosedWindowCommand = new DelegateCommand(ClosedWindowAction);
+
+            SelectedItemChangedCommand = new DelegateCommand<object>(executeMethod: PlotFunction);
+
+            ExportDataCommand = new DelegateCommand(OnExportData, CanExportData);
         }
+
+        
+        private bool CanExportData()
+        {
+            return !string.IsNullOrEmpty(SelectedPlan) && ScanDataCollection.Count != 0;
+        }
+
+        private void OnExportData()
+        {
+            foreach (var item in ScanDataCollection.GroupBy(x => x.Direction))
+            {
+                if(item.Key == "PDD")
+                {
+                    // Export PDD
+                    ExportFiles(item.ToArray(), true);
+                }
+                else
+                {
+                    foreach (var item2 in ScanDataCollection.GroupBy(x => x.FieldSize))
+                    {
+                        ExportFiles(item2.ToArray(), false);
+                    }
+                }
+            }
+        }
+
+        private void ExportFiles(ScanData[] scanData, bool isPdd)
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string fileName = $"{scanData.First().Direction}_" + 
+                $"{(isPdd ? "All FS" : "FS_" + scanData.First().FieldSize.ToString())}" +
+                ".csv";
+
+            using (StreamWriter sw = new StreamWriter(Path.Combine(path, fileName)))
+            {
+                // Field Size, Header line
+                sw.WriteLine($"Pos, " +
+                    $"{(isPdd ? string.Join(", ", scanData.Select(x => x.FieldSize)) : string.Join(", ", scanData.Select(x => x.Depth)))}");
+                for (int i = 0; i < scanData.First().DataPoints.Count; i++)
+                {
+                    sw.WriteLine($"{scanData.First().DataPoints[i].Position:F2}, " 
+                        + $"{string.Join(", ", scanData.Select(x => x.DataPoints[i].Dose))}");
+                }
+
+                sw.Flush();
+            }
+        }
+
+        private bool CanPlot(object arg)
+        {
+            ScanData sd = arg as ScanData;
+            return null == sd ? false : true;
+        }
+
+        private void PlotFunction(object obj)
+        {
+            if (!CanPlot(obj))
+            {
+                return;
+            }
+            ScanData sd = obj as ScanData;
+            if(sd.Direction == "PDD")
+            {
+                ProfileVisibility = Visibility.Collapsed;
+                PDDVisibility = Visibility.Visible;
+                PDDPlotModel.Series.Clear();
+
+                LineSeries ls = new LineSeries();
+                foreach (var item in sd.DataPoints)
+                {
+                    ls.Points.Add(new DataPoint(item.Position, item.Dose));
+                }
+                PDDPlotModel.Series.Add(ls);
+
+                PDDPlotModel.InvalidatePlot(true);
+
+            }
+            else if (sd.Direction == "Profile (x)")
+            {
+                PDDVisibility = Visibility.Collapsed;
+                ProfileVisibility = Visibility.Visible;
+                ProfilePlotModel.Series.Clear();
+
+                LineSeries ls = new LineSeries();
+                foreach (var item in sd.DataPoints)
+                {
+                    ls.Points.Add(new DataPoint(item.Position, item.Dose));
+                }
+
+                ProfilePlotModel.Series.Add(ls);
+                ProfilePlotModel.InvalidatePlot(true);
+            }
+        }
+
 
         private void ClosedWindowAction()
         {
@@ -138,12 +287,37 @@ namespace ModelValidation.ViewModels
                 }
             }
 
-
         }
 
         private ScanData GetProfileAtDepth(Beam beam, double dd)
         {
-            return new ScanData { Direction = "N/A" };
+            ScanData scanData = new ScanData()
+            {
+                FieldSize = GetFieldSize(beam),
+                Depth = dd,
+                Direction = "Profile (x)"
+            };
+
+            VVector startPos = new VVector((-scanData.FieldSize / 2 ) * 1.5, dd - 200, 0);
+
+            VVector endPos = new VVector((scanData.FieldSize / 2) * 1.5, dd - 200, 0);
+
+            double[] size = new double[(int)(scanData.FieldSize / 2 * 3)];
+
+            DoseProfile dp = beam.Dose.GetDoseProfile(startPos, endPos, size);
+
+            foreach (var pd in dp)
+            {
+                scanData.DataPoints.Add(new ScanDataPoint()
+                {
+                    Position = pd.Position.x,
+                    Dose = pd.Value
+                });
+            }
+
+            ExportDataCommand.RaiseCanExecuteChanged();
+
+            return scanData;
         }
 
         private ScanData GetPDD(Beam beam)
@@ -172,6 +346,10 @@ namespace ModelValidation.ViewModels
                     Dose = pd.Value
                 });
             }
+
+            ExportDataCommand.RaiseCanExecuteChanged();
+            SelectedItemChangedCommand.RaiseCanExecuteChanged();
+
 
             return scanData;
         }
